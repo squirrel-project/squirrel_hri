f6179a0e7a7c3af3aaa71a8fb263feb63b00868e
ipa-nhg <nadia.hammoudeh.garcia@ipa.fraunhofer.de>
2016-02-24 12:49:03 +0100

    solve libnite2 build issue

--- squirrel_person_tracker/create_nite2_link.bash
+++ squirrel_person_tracker/create_nite2_link.bash
@@ -1,15 +1,14 @@
 #!/bin/bash
-if [ $# != 1 ]; then
-	echo "Usage: $0 NITE_DIR" >&2
-	exit -1
-fi
-NITE_DIR=$1
+
+NITE_DIR=$(rospack find libnite2)/common/lib/NiTE2/
 echo NiTE_DIR = $NITE_DIR
+
 LINK_NAME=$HOME/.ros/NiTE2
 echo LINK_NAME = $LINK_NAME
-if [ -d $LINK_NAME -o -h $LINK_NAME ]; then
-echo "Link $LINK_NAME to $NITE_DIR already exists."
+
+if [ -d $LINK_NAME ]; then
+	echo  "Link $LINK_NAME to $NITE_DIR already exists."
 else
-ln -s $NITE_DIR $LINK_NAME
-echo "Created link $LINK_NAME to $NITE_DIR."
+	ln -s $NITE_DIR $LINK_NAME
+	echo "Created link $LINK_NAME to $NITE_DIR."
 fi
