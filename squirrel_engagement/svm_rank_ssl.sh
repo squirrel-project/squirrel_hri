a97a9bf10935e739dbf0a0826470355cbfbb5521
batikim09 <kimj@ydWLT-E-18-226.uibk.ac.at>
2016-04-11 14:00:53 +0200

    engagement_detection

---
+++ squirrel_engagement/svm_rank_ssl.sh
@@ -0,0 +1,2 @@
+java -jar LiveEngagementDetection.jar -mode:SVM -config:./config/vad.default.config.xml -ch:1 -rate:16000 -folder:./data/  -dev:0  -minDurationMSec:150 -feat_ext_config:./config/feat_ext.config -svm_config:./config/svm.rank.config -decayRate:0.5 -windowSize:5000
+
