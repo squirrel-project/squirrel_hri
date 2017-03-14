4c581c5d502d35d2e4acc185feea869d33d54fcb
Edith Langer <langer@acin.tuwien.ac.at>
2017-03-14 12:06:43 +0100

    backup commit. Actionserver is still troublesome

--- squirrel_people_tracker/src/follow_child.cpp
+++ squirrel_people_tracker/src/follow_child.cpp
@@ -21,14 +21,14 @@ ChildFollowingAction::ChildFollowingAction(std::string name) : as_(nh_, name, fa
   as_.registerPreemptCallback(boost::bind(&ChildFollowingAction::preemptCB, this));
 
   // subscribe to the data topic of interest
-  sub_ = nh_.subscribe("people_tracker_filter", 1, &ChildFollowingAction::analysisCB, this);
+  sub_ = nh_.subscribe("people_tracker_measurements", 1, &ChildFollowingAction::analysisCB, this);
   as_.start();
 }
 
 void ChildFollowingAction::goalCB()
 {
   goal_ = as_.acceptNewGoal();
-  nh_.createTimer(ros::Duration(goal_->time_standing_still), &ChildFollowingAction::timerCB, this);
+  //nh_.createTimer(ros::Duration(goal_->time_standing_still), &ChildFollowingAction::timerCB, this);
 }
 
 void ChildFollowingAction::timerCB(const ros::TimerEvent &event)
@@ -45,50 +45,68 @@ void ChildFollowingAction::preemptCB()
 
 void ChildFollowingAction::analysisCB(const people_msgs::PositionMeasurementArray::ConstPtr &msg)
 {
+  ROS_INFO("Data received. processing ...");
   // make sure that the action hasn't been canceled
   if (!as_.isActive())
-    return;
-
-  for (size_t i = 0; i < msg->people.size(); ++i)
   {
-    if (msg->people[i].object_id == "test")
-      ;  // goal_->child_id_to_follow)
+    ROS_INFO("Action server is no longer active. Exiting.");
+    return;
+  }
+
+  int index = 0;
+  double dist = 10.0;
+  for (size_t i = 0; i < msg->people.size(); i++)
+  {
+    double k = 10.0;
+    k = (sqrt(msg->people[i].pos.x*msg->people[i].pos.x + msg->people[i].pos.y*msg->people[i].pos.y));
+    ROS_INFO("k: %f, abs(k): %f", k, abs(k) );
+    if (k < dist)
     {
-      for (size_t j = 0; i < goal_->target_locations.size(); ++j)
-      {
-        double distx = pow((goal_->target_locations[j].x - msg->people[i].pos.x), 2);
-        double disty = pow((goal_->target_locations[j].y - msg->people[i].pos.y), 2);
-        if ((abs(sqrt(distx - disty))) < target_distance_)
-        {
-          ROS_INFO("%s: Succeeded", action_name_.c_str());
-          ROS_INFO("Child is at (x,y): (%f, %f)", msg->people[i].pos.x, msg->people[i].pos.y);
-          // set the action state to succeeded
-          as_.setSucceeded(result_);
-        }
-      }
-      // calculate a point between the child and the robot
-      // (x, y) = (c_x +- d/sqrt(2), c_y +- d/sqrt(2))
-      point_.header.stamp = ros::Time::now();
-      point_.pose.position.x = msg->people[i].pos.x - distance_ / sqrt(2);
-      point_.pose.position.y = msg->people[i].pos.y - distance_ / sqrt(2);
-      ROS_INFO("Setting nav goal to (x, y): (%f, %f)", point_.pose.position.x, point_.pose.position.y);
+      index = i;
+      dist = k;
+    }
+  }
+  ROS_INFO("smallest distance is: %f from index %d", dist, index);
 
-      // store last position in result_
-      result_.final_location.header.stamp = point_.header.stamp;
-      result_.final_location.pose.position.x = point_.pose.position.x;
-      result_.final_location.pose.position.y = point_.pose.position.y;
+  // calculate a point between the child and the robot
+  // (x, y) = (c_x +- d/sqrt(2), c_y +- d/sqrt(2))
+  point_.header.stamp = ros::Time::now();
+  point_.pose.position.x = msg->people[index].pos.x - 1.0 / sqrt(2);
+  point_.pose.position.y = msg->people[index].pos.y - 1.0 / sqrt(2);
+  ROS_INFO("Setting nav goal to (x, y): (%f, %f)", point_.pose.position.x, point_.pose.position.y);
 
-      move_base_goal_.target_pose.header.frame_id = "hokuyo_link";
-      move_base_goal_.target_pose.header.stamp = ros::Time::now();
+  // store last position in result_
+  result_.final_location.header.stamp = point_.header.stamp;
+  result_.final_location.pose.position.x = point_.pose.position.x;
+  result_.final_location.pose.position.y = point_.pose.position.y;
 
-      move_base_goal_.target_pose.pose.position.x = point_.pose.position.x;
-      move_base_goal_.target_pose.pose.position.y = point_.pose.position.y;
-      move_base_goal_.target_pose.pose.orientation.w = 1.0;
+  move_base_goal_.target_pose.header.frame_id = "hokuyo_link";
+  move_base_goal_.target_pose.header.stamp = ros::Time::now();
 
-      ROS_INFO("Sending goal");
-      move_base_ac_->sendGoal(move_base_goal_);
+  move_base_goal_.target_pose.pose.position.x = point_.pose.position.x;
+  move_base_goal_.target_pose.pose.position.y = point_.pose.position.y;
+  move_base_goal_.target_pose.pose.orientation.w = 1.0;
 
-      ros::Duration(1.0).sleep();
+  ROS_INFO("Sending goal");
+  move_base_ac_->sendGoal(move_base_goal_);
+  ros::Duration(1.5).sleep();
+
+  ROS_INFO("%d",  goal_->target_locations.size());
+  for (size_t j = 0; j < goal_->target_locations.size(); j++)
+  {
+    double distance = 0.001;
+    double distx = 0.01;
+    double disty = 0.01;
+    distx = pow((goal_->target_locations[j].x - msg->people[index].pos.x), 2);
+    disty = pow((goal_->target_locations[j].y - msg->people[index].pos.y), 2);
+    distance = (sqrt(distx + disty));
+    ROS_INFO("Distance to target position is %f", distance);
+    if (abs(distance) < distance_)
+    {
+      ROS_INFO("Set success");
+      // set the action state to succeeded
+      as_.setSucceeded(result_);
+      return;
     }
   }
 }
