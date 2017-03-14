6b1bc5a61a6d19d12d68d1b4265156bdbc08820c
Edith Langer <langer@acin.tuwien.ac.at>
2017-03-14 03:17:23 +0100

    correctly handle frame. leg_detector needs to be set to hokuyo_link

--- squirrel_people_tracker/src/test.cpp
+++ squirrel_people_tracker/src/test.cpp
@@ -10,6 +10,8 @@
 #include <tf/tf.h>
 #include <tf/transform_listener.h>
 #include <tf/transform_datatypes.h>
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
@@ -27,9 +29,11 @@ public:
 
     //Topic you want to publish
     pub_ = n_.advertise<geometry_msgs::PoseStamped>("/published_topic", 1);
-
+    vis_pub_ = n_.advertise<visualization_msgs::Marker>( "visualization_marker", 0 );
     //Topic you want to subscribe
     sub_ = n_.subscribe("people_tracker_measurements", 1000, &SubscribeAndPublish::personCB, this);
+    last_goal_.position.x = 1000.0;
+    last_goal_.position.y = 1000.0;
 
   }
   ~SubscribeAndPublish()
@@ -42,44 +46,36 @@ public:
   {
     geometry_msgs::PoseStamped robot_pose, child_pose, tmp_pose, out_pose;
     move_base_msgs::MoveBaseGoal move_base_goal_;
-    int index = 0;
-    double dist = 10.0;
-    for (size_t i = 0; i < msg->people.size(); i++)
-    {
-      double k = 10.0;
-      k = (sqrt(msg->people[i].pos.x*msg->people[i].pos.x + msg->people[i].pos.y*msg->people[i].pos.y));
-      ROS_INFO("k: %f, fabs(k): %f", k, fabs(k) );
-      if (k < dist)
-      {
-        index = i;
-        dist = k;
-      }
-    }
-    ROS_INFO("smallest distance is: %f from index %d", dist, index);
-    ROS_INFO("msg contains %ld data points", msg->people.size());
+    
     if (msg->people.size() == 0)
       return;
     
-    ros::Time t = ros::Time(0);
-    /*
-    tf::StampedTransform transform;
-    tfl_.waitForTransform("hokuyo_link", "map", t, ros::Duration(1.0));
-    tfl_.lookupTransform("hokuyo_link", "map", t, transform);
-    */
-
-    // get the Euler angle (radian) between 
+    int index = 0;
     double alpha = 0.0;
-    alpha = atan2(msg->people[index].pos.x, msg->people[index].pos.y);
-
-    // calculate a point between the child and the robot
-    // (x, y) = (c_x +- d/sqrt(2), c_y +- d/sqrt(2))
     tmp_pose.header.stamp = ros::Time(0);
     tmp_pose.header.frame_id = "hokuyo_link";
-    tmp_pose.pose.position.x = msg->people[index].pos.x - distance_;
-    tmp_pose.pose.position.y = msg->people[index].pos.y - sin(alpha) * distance_ ;
+    tmp_pose.pose.position.x = msg->people[index].pos.x;
+    tmp_pose.pose.position.y = msg->people[index].pos.y;
     tmp_pose.pose.orientation =  tf::createQuaternionMsgFromYaw(alpha);
+    ROS_INFO("Person detected at (x, y): (%f, %f) hokuyo_link", tmp_pose.pose.position.x, tmp_pose.pose.position.y);
+
+// calculate a point between the child and the robot
+    alpha = atan2(tmp_pose.pose.position.y, tmp_pose.pose.position.x);
+    double k = sqrt(tmp_pose.pose.position.x * tmp_pose.pose.position.x + tmp_pose.pose.position.y * tmp_pose.pose.position.y);
+    
+    tmp_pose.header.stamp = ros::Time(0);
+    tmp_pose.header.frame_id = "hokuyo_link";
+    tmp_pose.pose.position.x = (k - distance_)*cos(alpha);
+    tmp_pose.pose.position.y = (k - distance_)*sin(alpha);
+    tmp_pose.pose.orientation = tf::createQuaternionMsgFromYaw(alpha);
+    ROS_INFO("x: %f", tmp_pose.pose.position.x);
+    ROS_INFO("y: %f", tmp_pose.pose.position.y);
+    ROS_INFO("Setting nav goal to (x, y): (%f, %f) hokuyo_link", tmp_pose.pose.position.x, tmp_pose.pose.position.y);
     
     try{
+      ros::Time now = ros::Time(0);
+      tfl_.waitForTransform("hokuyo_link", "map",
+                              now, ros::Duration(3.0));
       tfl_.transformPose("map", tmp_pose, out_pose);
     }
     catch (tf::TransformException ex){
@@ -87,8 +83,40 @@ public:
       ros::Duration(1.0).sleep();
       return;
     }
+    visualization_msgs::Marker marker;
+    marker.header.frame_id = "map";
+    marker.header.stamp = ros::Time(0);
+    marker.ns = "goal";
+    marker.id = 0;
+    marker.type = visualization_msgs::Marker::SPHERE;
+    marker.action = visualization_msgs::Marker::ADD;
+    marker.pose.position.x = out_pose.pose.position.x;
+    marker.pose.position.y = out_pose.pose.position.y;
+    marker.pose.position.z = out_pose.pose.position.z;
+    marker.pose.orientation.x = 0.0;
+    marker.pose.orientation.y = 0.0;
+    marker.pose.orientation.z = 0.0;
+    marker.pose.orientation.w = 1.0;
+    marker.scale.x = 0.1;
+    marker.scale.y = 0.1;
+    marker.scale.z = 0.1;
+    marker.color.a = 1.0; // Don't forget to set the alpha!
+    marker.color.r = 1.0;
+    marker.color.g = 0.0;
+    marker.color.b = 0.0; 
+    vis_pub_.publish(marker);
+    ros::Duration(2.0).sleep();
 
+    if ((last_goal_.position.x - out_pose.pose.position.x) < 0.25 && 
+        (last_goal_.position.y - out_pose.pose.position.y) < 0.25 )
+    {
+      ROS_INFO("current goal and last goal are close to each other. Do not send new goal");
+      return;
+    }
     ROS_INFO("Setting nav goal to (x, y): (%f, %f)", out_pose.pose.position.x, out_pose.pose.position.y);
+
+    last_goal_.position = out_pose.pose.position;
+    last_goal_.orientation = out_pose.pose.orientation;
 
     move_base_goal_.target_pose.header.frame_id = out_pose.header.frame_id;
     move_base_goal_.target_pose.header.stamp = out_pose.header.stamp;
@@ -97,16 +125,16 @@ public:
     move_base_goal_.target_pose.pose.orientation = out_pose.pose.orientation;
 
     ROS_INFO("Sending goal");
-    this->ac_->sendGoal(move_base_goal_);
-    ros::Duration(2.0).sleep();
-    
-    pub_.publish(out_pose);
+    //this->ac_->sendGoal(move_base_goal_);
   }
 
 private:
   ros::NodeHandle n_; 
   ros::Publisher pub_;
   ros::Subscriber sub_;
+  ros::Publisher vis_pub_;
+  geometry_msgs::Pose last_goal_;
+ 
   actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> *ac_;
   tf::TransformListener tfl_;
   double distance_;
